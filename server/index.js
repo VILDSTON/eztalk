4625e6cb4c5ffedd3dee28a1045bb73992f2c61b
VILDSTON <xojaevsulaymon@gmail.com>
2026-09-11 21:35:35 +0500

    fix(server): resolve express path-to-regexp syntax error for SPA wildcard route

--- server/index.js
+++ server/index.js
@@ -1634,7 +1634,7 @@ io.on('connection', (socket) => {
 const DIST_PATH = path.join(__dirname, '../dist');
 if (fs.existsSync(DIST_PATH)) {
   app.use(express.static(DIST_PATH));
-  app.get('*', (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(DIST_PATH, 'index.html'));
   });
 }
