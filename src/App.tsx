2952b834538b9597fb1721378c26c1d403f7c462
VILDSTON <xojaevsulaymon@gmail.com>
2026-09-13 15:27:34 +0500

    fix(router): revert incorrect /:lang wrapping

--- src/App.tsx
+++ src/App.tsx
@@ -2280,12 +2280,10 @@ function MainApp() {
 
   return (
     <Routes>
-      <Route path="/:lang">
-        <Route path="login" element={isAuth ? <Navigate to={`/${lang}/t/direct`} replace /> : authContent} />
-        <Route path="t/*" element={!isAuth ? <Navigate to={`/${lang}/login`} replace /> : mainContent} />
-        <Route path="direct/*" element={<Navigate to={`/${lang}/t/direct`} replace />} />
-        <Route path="@:handle" element={<Navigate to={`/${lang}/t/direct`} replace />} />
-      </Route>
+      <Route path="login" element={isAuth ? <Navigate to={`/${lang}/t/direct`} replace /> : authContent} />
+      <Route path="t/*" element={!isAuth ? <Navigate to={`/${lang}/login`} replace /> : mainContent} />
+      <Route path="direct/*" element={<Navigate to={`/${lang}/t/direct`} replace />} />
+      <Route path="@:handle" element={<Navigate to={`/${lang}/t/direct`} replace />} />
       <Route path="*" element={<Navigate to={`/${lang}/${isAuth ? 't/direct' : 'login'}`} replace />} />
     </Routes>
   );
